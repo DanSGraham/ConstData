ce7eede810f7e02d161f89c04a5230497f3f1c10
DanSGraham <dan.s.graham@gmail.com>
2015-08-24 22:45:19 -0400

    Correct Tabs

--- ConstantData/app/src/main/java/dan/constantdata/MoleculeSearch.java
+++ ConstantData/app/src/main/java/dan/constantdata/MoleculeSearch.java
@@ -7,6 +7,14 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 
 /**
@@ -18,31 +26,19 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MoleculeSearch extends Fragment {
-    /** // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
 
+
+/**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MoleculeSearch.
+     * @return A new instance of fragment MoleculeSearch.*/
 
     // TODO: Rename and change types and number of parameters
-    public static MoleculeSearch newInstance(String param1, String param2) {
+    public static MoleculeSearch newInstance() {
         MoleculeSearch fragment = new MoleculeSearch();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,18 +46,66 @@ public class MoleculeSearch extends Fragment {
     public MoleculeSearch() {
         // Required empty public constructor
     }
-    */
+
+
+    public HashMap<String, JSONObject> queryDatabaases(String queryString){
+        //This is the method that will return an array of JSON objects associated with each database
+        //searched. I actually need a hashmap with the key being the name of teh database searched
+        //and the value being the json object. This will make formatting way easier.
+
+        //The following is for designing the layout only
+
+        HashMap<String, JSONObject> query_results = new HashMap<String, JSONObject>();
+
+        JSON_Adapter json_adapter = new JSON_Adapter();
+        try{
+            JSONObject json_data = json_adapter.get_JSON_object(new URL("http://www.nist.gov/srd/srd_data/srd144_Atomic_Weights_and_Isotopic_Compositions_for_All_Elements.json"));
+            query_results.put("Atomic Weights and Isotopic Compositions", json_data);
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        return query_results;
+    }
+
+    public LinearLayout isotopeDBView(JSONObject isotopeObject){
+        LinearLayout ll = new LinearLayout(this.getActivity());
+        ll.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        TextView title = new TextView(this.getActivity());
+        title.setText("TITLE HERE");
+        ll.addView(title);
+        return ll;
+
+    }
+
+    public void updateContentView(ViewGroup contentArea, HashMap<String, JSONObject> searchResults){
+        for (String key: searchResults.keySet()){
+            if (key == "Atomic Weights and Isotopic Compositions"){
+                contentArea.addView(isotopeDBView(searchResults.get(key)));
+            }
+        }
+
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_molecule_search, container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState){
+        //HashMap <String, JSONObject> results = queryDatabaases("TESTSTRING");
+        //LinearLayout content_section = (LinearLayout) getView().findViewById(R.id.content_layout);
+        //updateContentView(content_section, results);
     }
 
 }
--- LabPal/app/src/main/java/danandzach/labpal/MainActivity.java
+++ LabPal/app/src/main/java/danandzach/labpal/MainActivity.java
@@ -1,17 +1,51 @@
 package danandzach.labpal;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements android.support.v7.app.ActionBar.TabListener{
+
+    //Implementation of TabListener -D
+
+
+    public void onTabSelected(android.support.v7.app.ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft){
+
+    }
+
+    public void onTabUnselected(android.support.v7.app.ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft){
+
+    }
+
+    public void onTabReselected(android.support.v7.app.ActionBar.Tab tab, android.support.v4.app.FragmentTransaction ft){
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+
+        //The following method of tabs is deprecated. In API 21 the correct way to use tab
+        //navigation is TabLayout however there are not many devices on android 5 so I am
+        //Using this backwards compatible method for now. -D
+        actionBar.setNavigationMode(android.support.v7.app.ActionBar.NAVIGATION_MODE_TABS);
+
+        for (int i = 0; i < 4; i++){
+            android.support.v7.app.ActionBar.Tab tab = actionBar.newTab();
+            tab.setText("Tab" + i);
+            tab.setTabListener(this);
+            actionBar.addTab(tab);
+        }
+
 
         Intent splash = new Intent(MainActivity.this, SplashScreen.class);
         startActivity(splash);
