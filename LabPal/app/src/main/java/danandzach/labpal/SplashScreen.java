3b595a5366a681ca7c8efd8a8e68f83239d2b5bf
Zachary Ratliff <ratliffzachary@yahoo.com>
2015-08-31 17:31:46 -0500

    Splash screen is monitored now. Doesn't show everytime app closes

--- LabPal/app/src/main/java/danandzach/labpal/SplashScreen.java
+++ LabPal/app/src/main/java/danandzach/labpal/SplashScreen.java
@@ -19,49 +19,58 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_splash_screen);
+        if(Data.getAtomic_mass_data() == null || Data.getIonization_data() == null || Data.getConstants_data() == null){
+            setContentView(R.layout.activity_splash_screen);
 
-        //Initializes the periodic table -D
-        Data.initPeriodicTable();
+            //Initializes the periodic table -D
+            Data.initPeriodicTable();
 
-        JSON_Adapter atomic_adapter = new JSON_Adapter();
-        JSON_Adapter ion_adapter = new JSON_Adapter();
-        JSON_Adapter const_adapter = new JSON_Adapter();
+            JSON_Adapter atomic_adapter = new JSON_Adapter();
+            JSON_Adapter ion_adapter = new JSON_Adapter();
+            JSON_Adapter const_adapter = new JSON_Adapter();
 
-        Data.constants_is = getResources().openRawResource(R.raw.constants);
-        Data.ionization_is = getResources().openRawResource(R.raw.ionizations);
-        Data.atomic_mass_is = getResources().openRawResource(R.raw.atomic_mass);
-        try {
-            if(isNetworkConnected()){
+            Data.constants_is = getResources().openRawResource(R.raw.constants);
+            Data.ionization_is = getResources().openRawResource(R.raw.ionizations);
+            Data.atomic_mass_is = getResources().openRawResource(R.raw.atomic_mass);
+            try {
+                if(isNetworkConnected()){
                     Data.setNetwork_connection(true);
                     Log.v("CONNECTION STATUS: ", "CONNECTED");
                 }else{
                     Log.v("CONNECTION STATUS: ", "NOT CONNECTED");
                 }
 
-            atomic_adapter.get_JSON_object(new URL(Data.getUrl_atomic_mass()));
-            ion_adapter.get_JSON_object(new URL(Data.getUrl_ionization()));
-            const_adapter.get_JSON_object(new URL(Data.getUrl_constants()));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        Thread splash = new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                try {
-                    //5 and a half seconds to load the databases
-                    this.sleep(6000);
-                    Intent start = new Intent(SplashScreen.this, MainActivity.class);
-                    start.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(start);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                atomic_adapter.get_JSON_object(new URL(Data.getUrl_atomic_mass()));
+                ion_adapter.get_JSON_object(new URL(Data.getUrl_ionization()));
+                const_adapter.get_JSON_object(new URL(Data.getUrl_constants()));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             }
-        };
-        splash.start();
+            Thread splash = new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    try {
+                        //5 and a half seconds to load the databases
+                        this.sleep(6000);
+                        Intent start = new Intent(SplashScreen.this, MainActivity.class);
+                        start.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(start);
+                        finish();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            splash.start();
+        }else{
+            Intent start = new Intent(SplashScreen.this, MainActivity.class);
+            start.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(start);
+            finish();
+        }
+
 
     }
 
