d7a2958853c6f25e8ae5c73bb07623cf36fe11e0
Zachary Ratliff <ratliffzachary@yahoo.com>
2015-09-03 12:28:19 -0500

    Fixed action bar along with the ability to share notes

--- LabPal/app/src/main/java/danandzach/labpal/MainActivity.java
+++ LabPal/app/src/main/java/danandzach/labpal/MainActivity.java
@@ -97,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -118,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
 
 class NavigationTabsListener implements android.support.v7.app.ActionBar.TabListener{
