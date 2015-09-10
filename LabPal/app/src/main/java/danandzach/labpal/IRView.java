a648beb2930b8a4179e6e056156713200ae59c91
Zachary Ratliff <ratliffzachary@yahoo.com>
2015-09-10 11:26:19 -0500

    Test for the charts

--- LabPal/app/src/main/java/danandzach/labpal/IRView.java
+++ LabPal/app/src/main/java/danandzach/labpal/IRView.java
@@ -8,6 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,7 +52,32 @@ public class IRView extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_irview, container, false);
+        View v = inflater.inflate(R.layout.fragment_irview, container, false);
+        BarChart display_chart = (BarChart)v.findViewById(R.id.ir_chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataSet = new BarDataSet(entries, "JUST A TEST");
+
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Jan");
+        labels.add("Feb");
+        labels.add("Mar");
+        labels.add("April");
+        labels.add("June");
+        labels.add("July");
+
+        BarData data = new BarData(labels, dataSet);
+        display_chart.setData(data);
+
+        display_chart.setDescription("Test for IR Viewer");
+        return v;
     }
 
 
